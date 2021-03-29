f07247a2d5049ea24b0655a1b735a5b1053372bb
Martin Mullins <martin.mullins942+git@gmail.com>
2021-03-29 19:54:42 +0100

    add the quake 3 demo

---
+++ b.sh
@@ -0,0 +1,24 @@
+em++ \
+  -O3 \
+  --emrun \
+  --preload-file ./media@/media \
+  -s LLD_REPORT_UNDEFINED \
+  -s FULL_ES2=1 \
+  -s DISABLE_EXCEPTION_CATCHING=0 \
+  -s SAFE_HEAP=0 \
+  -s SAFE_HEAP=0 \
+  -s ALLOW_MEMORY_GROWTH=1 \
+  -s STACK_OVERFLOW_CHECK=2 \
+  -s ASSERTIONS=1 \
+  -s ERROR_ON_UNDEFINED_SYMBOLS=1 \
+  -I/tmp/osmesa/include \
+  -L/tmp/osmesa/lib \
+  -I/home/marto/irrlicht/irr-osmesa/include \
+  -L/home/marto/irrlicht/irr-osmesa/lib/Linux \
+  -o main.html \
+  main.cpp \
+  -lIrrlicht \
+  -lOSMesa \
+  -lglapi \
+  -lm \
+  -s USE_ZLIB=1
